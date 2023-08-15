4825e1648354199890b3068dd249fd8a8d146aab
Batuhan <57671610+BatuhanKeskinsoy@users.noreply.github.com>
2023-08-15 12:04:54 +0300

    emlak haber detail components

---
+++ src/components/EmlakHaberleri/HaberDetay/HaberSidebar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function HaberSidebar() {
+    return (
+        <div className='flex-[1_1_25%] lg:mt-0 mt-6 bg-white'>
+            
+        </div>
+    )
+}
+
+export default HaberSidebar
