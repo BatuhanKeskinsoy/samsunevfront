838fa9abd9f8fa10114fcb23803b6b2a6421c4d1
Batuhan <57671610+BatuhanKeskinsoy@users.noreply.github.com>
2023-08-15 22:07:24 +0300

    about, contact and corporate membership

---
+++ src/components/KurumsalUyelik/Banner.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Banner() {
+    return (
+        <section className='bg-gradient-to-b from-blue-950 to-site h-[500px] shadow-lg text-white p-6'>
+            Banner
+        </section>
+    )
+}
+
+export default Banner
