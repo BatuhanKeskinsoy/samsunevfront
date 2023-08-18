d99bc0361e5d805090b403155a5b489a097ea916
Batuhan <57671610+BatuhanKeskinsoy@users.noreply.github.com>
2023-08-18 15:05:07 +0300

    Login fix

--- src/data/Auth/LoginResponse.js
+++
@@ -1,7 +0,0 @@
-export const LoginResponse = await fetch('https://panel.samsunev.com/api/v1/auth/login', {
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(Logindata)
-});
